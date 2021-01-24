75fbaee36b3e2f22a3cf4d0f5c5aaf03026cf2d5
Selim Yüksel <yukselselim26@hotmail.com>
2021-01-24 19:48:24 +0300

    a

---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> icecekler = new MyDictionary<string, string>();
+
+            icecekler.Add("1", "Coca-Cola");
+            icecekler.Add("2", "Pepsi");
+
+            icecekler.GetAll();
+        }
+    }
+}
